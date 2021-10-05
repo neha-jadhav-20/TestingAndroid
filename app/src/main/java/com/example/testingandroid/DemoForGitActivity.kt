9806a5e7856a63947e70c461b4b6760f23e76359
Neha Jadhav <najadhav2000@gmail.com>
2021-10-05 12:53:15 +0530

    new activity added

---
+++ app/src/main/java/com/example/testingandroid/DemoForGitActivity.kt
@@ -0,0 +1,11 @@
+package com.example.testingandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DemoForGitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo_for_git)
+    }
+}
